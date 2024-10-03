0d9121971eb1c0b52d077acf3d98cc6742c6db6d
Abhishek Dubey <abhishekdubey331@gmail.com>
2024-10-03 23:55:59 +0530

    Day 3 of Blind 75: Implemented contains duplicate

--- src/array/Q3ContainsDuplicate.kt
+++ src/array/Q3ContainsDuplicate.kt
@@ -1,4 +1,26 @@
 package array
 
-class Q3ContainsDuplicate {
+/***
+ *
+ * 217. Contains Duplicate
+ * Given an integer array nums, return true if any value appears at least twice in the array,
+ * and return false if every element is distinct.
+ *
+ *
+ */
+fun containsDuplicate(array: IntArray): Boolean {
+    val set = mutableSetOf<Int>()
+    for (num in array) {
+        val added = set.add(num)
+        if (added.not()) {
+            return true
+        }
+    }
+    return false
+}
+
+fun main() {
+    val array = intArrayOf(1, 2, 3, 1)
+    val result = containsDuplicate(array)
+    println("Does array contains duplicate: $result")
 }
